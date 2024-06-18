34b4d368da11a7b6068b7d11d0b9e59eff34bb0f
Burmaja Milan <burmajam@gmail.com>
2024-06-18 16:45:36 +0100

    Use extreme with backpressure

---
+++ lib/kelvin/listener.ex
@@ -0,0 +1,22 @@
+defmodule Kelvin.Listener do
+  @moduledoc false
+  use Extreme.ListenerWithBackPressure
+
+  @impl Extreme.ListenerWithBackPressure
+  def on_init(opts) do
+    state = %{
+      producer: Keyword.fetch!(opts, :producer),
+      get_stream_position_fun: Keyword.fetch!(opts, :get_stream_position_fun)
+    }
+
+    {:ok, state}
+  end
+
+  @impl Extreme.ListenerWithBackPressure
+  def get_last_event(_stream_name, %{} = state),
+    do: state.get_stream_position_fun.()
+
+  @impl Extreme.ListenerWithBackPressure
+  def process_push(push, _stream_name, %{} = state),
+    do: GenServer.call(state.producer, {:on_event, push})
+end
